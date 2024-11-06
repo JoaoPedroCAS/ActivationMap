fa1565d8f4d14022f56c6a1f4f55b0efe0720d4f
JoaoPedroCAS <joaosa@alunos.utfpr.edu.br>
2024-11-06 15:00:54 -0300

    Primeiros mapas de ativação

--- resnet50_dtd_texture_svm_rskf.py
+++ resnet50_dtd_texture_svm_rskf.py
@@ -20,7 +20,7 @@ model = Model(inputs=resnet.input, outputs=activation_layer.output)
 
 final_dense = resnet.get_layer('predictions')
 W = final_dense.get_weights()[0]
-image = 0
+cont = 0
 while True:
     img = image.load_img(np.random.choice(image_files), target_size=(224,224))
     x = preprocess_input(np.expand_dims(img, 0))
@@ -30,7 +30,7 @@ while True:
     classnames = decode_predictions(probs)[0]
     print(classnames)
     classnames = classnames[0][1]
-    prin(classnames)
+    print(classnames)
     pred = np.argmax(probs[0])
 
     w = W[:, pred]
@@ -44,10 +44,10 @@ while True:
     plt.imshow(cam, cmap='jet', alpha=0.5)
     plt.subplot(1,2,2)
     plt.imshow(img)
-    plt.title(classname)
-    plt.savefig(f"/home/PreProjeto/Code/ActivationMap{image}.jpg")
+    plt.title(classnames)
+    plt.savefig(f"/home/joao.p.c.a.sa/PreProjeto/Code/ActivationMap{cont}.jpg")
 
     ans = input("Continue? (Y/n)")
-    if ans and ans[0].lowe() == 'n':
+    if ans and ans[0].lower() == 'n':
         break
-    image += 1
+    cont += 1
