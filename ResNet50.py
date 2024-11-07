0778bd90d6c72ba9fd78362d014e49a4dfd9ac46
JoaoPedroCAS <joaosa@alunos.utfpr.edu.br>
2024-11-07 19:59:37 -0300

    Activation Map Layer 0

--- ResNet50.py
+++ ResNet50.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 # Define o caminho para o seu dataset e para salvar as imagens dos mapas de ativação
 dataset_path = '/home/joao.p.c.a.sa/PreProjeto/Datasets'
-output_dir = '/home/joao.p.c.a.sa/PreProjeto/Code/activation_maps'  # Diretório para salvar as imagens dos mapas de ativação
+output_dir = '/home/joao.p.c.a.sa/PreProjeto/Code/activation_maps_layer_0'  # Diretório para salvar as imagens dos mapas de ativação
 os.makedirs(output_dir, exist_ok=True)
 print("Diretorios Criados")
 
@@ -21,23 +21,24 @@ class ResNet50FeatureExtractor(nn.Module):
     def __init__(self):
         super(ResNet50FeatureExtractor, self).__init__()
         self.model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
-        self.model = nn.Sequential(*list(self.model.children())[:-2])  # Remove a camada de classificação
-    
+        self.model = nn.Sequential(*list(self.model.children())[:-9])  # Remove a camada de classificação
+
     def forward(self, x):
         return self.model(x)
 
-# Definir transformações para pré-processamento das imagens
+# Define transformações para pré-processamento das imagens
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 print("Transformações Definidas")
-# Carregar e pré-processar as imagens
+# Função para carregar imagens
 def load_images_from_folder(folder):
     images = []
     labels = []
     original_images = []  # Para armazenar as imagens originais
+    filenames = []
     for label, subfolder in enumerate(os.listdir(folder)):
         subfolder_path = os.path.join(folder, subfolder)
         if os.path.isdir(subfolder_path):
@@ -49,53 +50,73 @@ def load_images_from_folder(folder):
                     img = transform(img)
                     images.append(img)
                     labels.append(label)
+                    filenames.append(filename)
                 except Exception as e:
                     print(f"Erro ao carregar a imagem {img_path}: {e}")
-    return images, labels, original_images
+    return images, labels, original_images, filenames
 
 # Carregar o dataset
-images, labels, original_images = load_images_from_folder(dataset_path)
+images, labels, original_images, filenames = load_images_from_folder(dataset_path)
 images_tensor = torch.stack(images)
 print("Imagens Carregadas")
-# Extrair características e gerar mapas de ativação
+
+# Adicionar hook na camada X
+def hook_fn(module, input, output):
+    # Aqui podemos salvar ou processar as ativações, que são as saídas da camada
+    return output
+
+# Configuração do dispositivo
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = ResNet50FeatureExtractor().to(device)
 model.eval()
 print("Modelo Avaliado")
+# Registra o hook na camada X
+layer_to_hook = model.model[0]
+hook = layer_to_hook.register_forward_hook(hook_fn)
+print("Hook feito")
+
+# Inicializar lista para armazenar as características
 features = []
+
+# Extração de características e geração de mapas de ativação
+target_filenames = [f'c{str(i+1).zfill(2)}_001_a_w01.png' for i in range(20)]
+print(target_filenames)
+
 for i in range(len(images_tensor)):
     img = images_tensor[i].unsqueeze(0).to(device)
-    
+
     with torch.no_grad():
-        activation = model(img)  # Extraí a ativação da última camada convolucional
+        activation = model(img)  # Extrai as ativações da camada 7
         feature = activation.cpu().numpy().flatten()
         features.append(feature)
-    print("Features Extraidos")
-    # Gera o mapa de ativação
-    activation_map = activation.squeeze(0).mean(dim=0).cpu().numpy()  # Calcula a média dos mapas de ativação
-    
-    # Normaliza o mapa de ativação para [0, 1] para visualização
-    activation_map = (activation_map - activation_map.min()) / (activation_map.max() - activation_map.min())
-    
-    # Exibir a imagem original e o mapa de ativação sobreposto
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-    
-    # Subplot 1: Imagem original
-    axes[0].imshow(original_images[i])
-    axes[0].axis('off')
-    axes[0].set_title('Imagem Original')
-    
-    # Subplot 2: Imagem com mapa de ativação sobreposto
-    axes[1].imshow(original_images[i], alpha=0.8)
-    axes[1].imshow(activation_map, cmap='jet', alpha=0.3)  # Sobrepõe o mapa de ativação
-    axes[1].axis('off')
-    axes[1].set_title('Imagem com Mapa de Ativação')
 
-    # Salvar a imagem do mapa de ativação
-    activation_map_path = os.path.join(output_dir, f'activation_map_{i}.png')
-    plt.savefig(activation_map_path)
-    plt.close(fig)
-    print("Mapa Gerado")
+    # Gera o mapa de ativação
+    if filenames[i] in target_filenames:
+        print(filenames[i])
+        activation_map = activation.squeeze(0).mean(dim=0).cpu().numpy()  # Calcula a média dos mapas de ativação
+
+        # Normaliza o mapa de ativação para [0, 1] para visualização
+        activation_map = (activation_map - activation_map.min()) / (activation_map.max() - activation_map.min())
+
+        # Exibir a imagem original e o mapa de ativação sobreposto
+        fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+
+        # Subplot 1: Imagem original
+        axes[0].imshow(original_images[i])
+        axes[0].axis('off')
+        axes[0].set_title('Imagem Original')
+
+        # Subplot 2: Imagem com mapa de ativação sobreposto
+        axes[1].imshow(original_images[i], alpha=0.8)
+        axes[1].imshow(activation_map, cmap='jet', alpha=0.3)  # Sobrepõe o mapa de ativação
+        axes[1].axis('off')
+        axes[1].set_title('Imagem com Mapa de Ativação')
+                # Salvar a imagem do mapa de ativação
+        activation_map_path = os.path.join(output_dir, f'{filenames[i]}_activation_map.png')
+        plt.savefig(activation_map_path)
+        plt.close(fig)
+
+print("Features extraídas")
 # Converter lista de características e rótulos em arrays
 features = np.array(features)
 labels = np.array(labels)
@@ -109,7 +130,7 @@ recall_scores = []
 precision_scores = []
 
 for fold, (train_index, test_index) in enumerate(cv.split(features, labels), 1):
-    print("Executando LDA com RSKF")
+    print("Iniciainado o LDA com RSKF")
     X_train, X_test = features[train_index], features[test_index]
     y_train, y_test = labels[train_index], labels[test_index]
 
@@ -125,6 +146,8 @@ for fold, (train_index, test_index) in enumerate(cv.split(features, labels), 1):
     recall = np.mean([report[str(i)]['recall'] for i in range(len(np.unique(labels))) if str(i) in report])
     precision = np.mean([report[str(i)]['precision'] for i in range(len(np.unique(labels))) if str(i) in report])
 
+    print(f"Acurácia: {accuracy}\nF1-Score: {f1}\nRecall: {recall}\nPrecision: {precision}\n----------")
+    
     accuracy_scores.append(accuracy)
     f1_scores.append(f1)
     recall_scores.append(recall)
--- model.py
+++ model.py
@@ -1,5 +1,18 @@
 import torchvision.models as models
+import torch.nn as nn
+
 
 model = models.resnet50(weights = models.ResNet50_Weights.IMAGENET1K_V1)
+model = nn.Sequential(*list(model.children())[:-9])
+# Imprimir todas as camadas e seus detalhes
 for name, module in model.named_children():
-	print(f"Camada: {name}, Tipo:: {module.__class__.__name__}")
+    print(f"Camada: {name}, Tipo: {module.__class__.__name__}")
+    
+    # Inspecionando os parâmetros da camada
+    for param_name, param in module.named_parameters():
+        print(f"  Parâmetro: {param_name} - Forma: {param.shape}")
+        
+    # Se a camada tem um método forward, você pode verificar sua configuração
+    if hasattr(module, 'forward'):
+        print(f"  Método Forward: {module.forward}")
+    print("-" * 50)  # Separador entre as camadas
