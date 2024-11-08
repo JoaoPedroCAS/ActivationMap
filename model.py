4da67e479bc2fa9362f5bd3eb03f03fe7e9fae68
JoaoPedroCAS <joaosa@alunos.utfpr.edu.br>
2024-11-08 13:13:10 -0300

    Activation Map Layer 0 and Layer 0 with GAP

--- model.py
+++ model.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 
 
 model = models.resnet50(weights = models.ResNet50_Weights.IMAGENET1K_V1)
-model = nn.Sequential(*list(model.children())[:-8])
+model = nn.Sequential(*list(model.children())[:-9])
+model.add_module("GAP", nn.AdaptiveAvgPool2d((1, 1)))
 # Imprimir todas as camadas e seus detalhes
 for name, module in model.named_children():
     print(f"Camada: {name}, Tipo: {module.__class__.__name__}")
